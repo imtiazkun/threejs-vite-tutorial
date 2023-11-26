b0aa08e2128b06c9382537acb65b613c12b0e57a
Imtiaz <imtiaz@zeltatech.com>
2023-11-26 23:42:12 +0600

    finished

--- main.js
+++ main.js
@@ -1,84 +1,100 @@
-import './style.css'
+import "./style.css";
 
 // import three as THREE
-import * as THREE from 'three'
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
 
+const loader = new GLTFLoader();
 
 // Scene
-const scene = new THREE.Scene()
+const scene = new THREE.Scene();
 // Object
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0x77ba66 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// const geometry = new THREE.BoxGeometry(1, 1, 1);
+// const material = new THREE.MeshBasicMaterial({ color: 0x77ba66 });
+// const mesh = new THREE.Mesh(geometry, material);
+// scene.add(mesh);
+
+// Load models
+loader.load(
+  // resource URL
+  "/shiba/scene.gltf",
+  // called when the resource is loaded
+  function (gltf) {
+    scene.add(gltf.scene);
+
+    // gltf.animations; // Array<THREE.AnimationClip>
+    // gltf.scene; // THREE.Group
+    // gltf.scenes; // Array<THREE.Group>
+    // gltf.cameras; // Array<THREE.Camera>
+    // gltf.asset; // Object
+  },
+  // called while loading is progressing
+  function (xhr) {
+    console.log((xhr.loaded / xhr.total) * 100 + "% loaded");
+  },
+  // called when loading has errors
+  function (error) {
+    console.log("An error happened");
+  }
+);
+
+// Light
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+scene.add(directionalLight);
+
 // Camera
 const sizes = {
   width: window.innerWidth,
-  height: window.innerHeight
-}
+  height: window.innerHeight,
+};
 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
-camera.position.z = 3
-scene.add(camera)
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
 
 // Renderer
-const canvas = document.querySelector('.webgl')
+const canvas = document.querySelector(".webgl");
 const renderer = new THREE.WebGLRenderer({
-  canvas: canvas
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.render(scene, camera)
+  canvas: canvas,
+});
 
+renderer.setClearColor(0xffffff, 0);
+renderer.setSize(sizes.width, sizes.height);
+renderer.render(scene, camera);
 
-// write a function to make the cube rotate
-const colors = [
-  0x77ba66,
-  0x66baba,
-  0xba6677,
-  0x6666ba,
-  0xba66ba
-]
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
 
 const tick = () => {
-  // Update objects
-  mesh.rotation.x += 0.01
-  mesh.rotation.y += 0.01
-  mesh.rotation.z += 0.01
-  // change mesh color
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  mesh.material.color.set(randomColor)
   // Render
-  renderer.render(scene, camera)
-
+  renderer.render(scene, camera);
+  controls.update();
   // Call tick again on the next frame
-  window.requestAnimationFrame(tick)
-}
+  window.requestAnimationFrame(tick);
+};
 
-tick()
-
-
+tick();
 
 window.addEventListener("resize", () => {
   // Update sizes
-  sizes.width = window.innerWidth
-  sizes.height = window.innerHeight
+  sizes.width = window.innerWidth;
+  sizes.height = window.innerHeight;
 
   // Update camera
-  camera.aspect = sizes.width / sizes.height
-  camera.updateProjectionMatrix()
+  camera.aspect = sizes.width / sizes.height;
+  camera.updateProjectionMatrix();
 
   // Update renderer
-  renderer.setSize(sizes.width, sizes.height)
-  renderer.render(scene, camera)
-})
-
+  renderer.setSize(sizes.width, sizes.height);
+  renderer.render(scene, camera);
+});
 
 // write a function to make the canvas full screen on double click
 window.addEventListener("dblclick", () => {
   if (!document.fullscreenElement) {
-    canvas.requestFullscreen()
+    canvas.requestFullscreen();
+  } else {
+    document.exitFullscreen();
   }
-  else {
-    document.exitFullscreen()
-  }
-})
+});
